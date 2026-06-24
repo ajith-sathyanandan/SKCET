bc4ffe1f7aa4ec77eb58fac84f665c2a24b07873
Ajith Sathyanandan <ajithsathyapriya@gmail.com>
2026-06-24 12:45:14 +0530

    feat: bootstrap Spring Boot backend for issue #5

---
+++ springapp/src/test/java/com/skcet/restaurantreservation/RestaurantReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skcet.restaurantreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
